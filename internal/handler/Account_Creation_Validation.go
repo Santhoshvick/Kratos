183d7cd300cccefd18fe0240a89b0914220f4ec6
Your Name <you@example.com>
2025-09-01 16:18:48 +0530

    newcommit

--- internal/handler/Account_Creation_Validation.go
+++ internal/handler/Account_Creation_Validation.go
@@ -6,13 +6,12 @@ import (
 	"strconv"
 )
 
-
-func BalanceValidation(availableBalance string) error{
+func BalanceValidation(availableBalance string) error {
 	avalBalance, err := strconv.ParseInt(availableBalance, 10, 64)
 	if err != nil {
 		log.Fatalf("Issue with conveting the available Balance %v", err)
 	}
-	if availableBalance==""{
+	if availableBalance == "" {
 		return errors.New("the available balance should not be less than 0 or should not be negative")
 	}
 
@@ -24,13 +23,12 @@ func BalanceValidation(availableBalance string) error{
 
 }
 
+func CurrencyValidation(currency string) error {
+	arr := []string{"RUPEE", "USD", "EURO", "DIRHAM"}
 
-func CurrencyValidation(currency string) error{
-	arr:=[]string{"RUPEE","USD","EURO","DIRHAM"}
-
-	for i:=0;i<len(arr);i++{
-		val:=arr[i]
-		if val==currency{
+	for i := 0; i < len(arr); i++ {
+		val := arr[i]
+		if val == currency {
 			return errors.New("please verify the currency Once.Allowed Curencies are USD  EURO  RUPEE   DIRHAM")
 		}
 	}
@@ -38,18 +36,14 @@ func CurrencyValidation(currency string) error{
 
 }
 
-
-func StatusValidations(status string) error{
-	arr:=[]string{"ACTIVE","SUSPENDED","CLOSED"}
-		for i:=0;i<len(arr);i++{
-		val:=arr[i]
-		if val==status{
+func StatusValidations(status string) error {
+	arr := []string{"ACTIVE", "SUSPENDED", "CLOSED"}
+	for i := 0; i < len(arr); i++ {
+		val := arr[i]
+		if val == status {
 			return errors.New("please verify the status Once.Allowed Status are ACTIVE  SUSPENDED  CLOSED ")
 		}
 	}
 	return nil
 
-
 }
-
-
